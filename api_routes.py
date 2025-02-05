c8d65ad50dbcda744fe05bc5006ea62bbdc0dba1
Jefferson-1987 <50810314+Jefferson-1987@users.noreply.github.com>
2025-02-04 23:33:31 -0300

    Update api_routes.py

--- api_routes.py
+++ api_routes.py
@@ -60,7 +60,7 @@ def reagendar_atendimento(arquivo, nome, inicio, ESF, departamento):
     df.loc[df["Nome"] == nome, "Status do Atendimento"] = "Reagendado"
     salvar_dados(df, arquivo)
     return f"Agendamento reagendado para a data {nova_hora} com sucesso!"
-print(incluir_agendamento(arquivo, "Jefferson Peres", "WhatsApp","2025-02-04 2:39:05 PM", "ESF Primavera", "Clínica Geral"))
+#print(incluir_agendamento(arquivo, "Jefferson Peres", "WhatsApp","2025-02-04 2:39:05 PM", "ESF Primavera", "Clínica Geral"))
 @api_blueprint.route('/receber_json', methods=['POST'])
 def receber_json():
     try:
